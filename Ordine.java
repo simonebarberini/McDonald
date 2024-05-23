2d7c0fb37f545097deef9070e43dbae8e7e7ea7e
Flavio <chiarappa_flavio.com>
2024-05-23 11:54:17 +0200

    push

--- Ordine.java
+++ Ordine.java
@@ -1,18 +1,26 @@
-class Ordine() {
+public class Ordine {
 
     Utente utente;
     Menu menu;
     Tavolo tavolo;
 
+    public Ordine() {
+        this.utente = utente;
+        this.menu = menu;
+        this.tavolo = tavolo;
+    }
+
     public void stampaOrdine() {
 
-        String username = getUsername();
-        int numTav = getNumTav();
-        double budget = getBudget();
-        int numPers = getPersone();
-        System.out.println("Ordine tavolo " + numTav + " per " + username + ", " +  numPers + " persone, budget" + budget + ":");
-        for(String i: prodotti) {
-            System.out.println(i);
-        }
+        // String username = getUsername();
+        // int numTav = getNumTav();
+        // double budget = getBudget();
+        // int numPers = getPersone();
+        // System.out.println(
+        //         "Ordine tavolo " + numTav + " per " + username + ", " + numPers + " persone, budget" + budget + ":");
+        
+        // for(String i: prodotti) {
+        //     System.out.println(i);
+        // }
     }
 }   
