e72fdba40ba66f8557dce596aa099712ff74bcd7
Flavio <chiarappa_flavio.com>
2024-05-23 11:30:19 +0200

    push della classe tavolo

---
+++ Tavolo.java
@@ -0,0 +1,33 @@
+public class Tavolo {
+    
+    private boolean occupato;
+    private int numeroTavolo;
+
+    public Tavolo(boolean occupato, int numeroTavolo) {
+        this.occupato = occupato;
+        this.numeroTavolo = numeroTavolo;
+    }
+
+    //getter e setter numero tavolo
+    public int getNumeroTavolo() {
+        return numeroTavolo;
+    }
+
+    public void setNumeroTavolo(int numeroTavolo) {
+        this.numeroTavolo = numeroTavolo;
+    }
+    
+    //getter e setter occupato
+    public boolean getOccupato() {
+        return occupato;
+    }
+
+    public void setOccupato(boolean occupato) {
+        this.occupato = occupato;
+    }
+    
+
+    
+}
+
+
